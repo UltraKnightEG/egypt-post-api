2485eeedc630bf547755a1d0c938368597d34b6c
UltraKnightEG <48820413+UltraKnightEG@users.noreply.github.com>
2025-05-20 17:53:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -67,6 +67,7 @@ def track():
         return jsonify({"error": f"حدث خطأ: {str(e)}"}), 500
 
 if __name__ == "__main__":
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
